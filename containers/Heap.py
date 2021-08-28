f608aa67a174d172e579ebcfc856896329df026e
Shreya Dasari <sdasari5090@scrippscollege.edu>
2021-08-27 20:45:14 -0700

    wrote Heap.py

--- containers/Heap.py
+++ containers/Heap.py
@@ -1,16 +1,18 @@
 '''
 This file implements the Heap data structure as a subclass of the BinaryTree.
-The book implements Heaps using an *implicit* tree with an *explicit* vector implementation,
-so the code in the book is likely to be less helpful than the code for the other data structures.
-The book's implementation is the traditional implementation because it has a faster constant factor
-(but the same asymptotics).
-This homework is using an explicit tree implementation to help you get more practice with OOP-style programming and classes.
+The book implements Heaps using an *implicit* tree with an *explicit* vector
+implementation, so the code in the book is likely to be less helpful than the
+code for the other data structures.
+The book's implementation is the traditional implementation because it has a
+faster constant factor (but the same asymptotics).
+This homework is using an explicit tree implementation to help you get more
+practice with OOP-style programming and classes.
 '''
 
-from containers.BinaryTree import BinaryTree, Node
+from BinaryTree import BinaryTree, Node
 
 
-class Heap():
+class Heap(BinaryTree):
     '''
     FIXME:
     Heap is currently not a subclass of BinaryTree.
@@ -24,19 +26,26 @@ class Heap():
         If xs is a list (i.e. xs is not None),
         then each element of xs needs to be inserted into the Heap.
         '''
+        
+        super().__init__()
+        self.size = 0
+        if xs:
+            self.insert_list(xs)
 
     def __repr__(self):
         '''
         Notice that in the BinaryTree class,
         we defined a __str__ function,
         but not a __repr__ function.
-        Recall that the __repr__ function should return a string that can be used to recreate a valid instance of the class.
+        Recall that the __repr__ function should return a string that can be
+        used to recreate a valid instance of the class.
         Thus, if you create a variable using the command Heap([1,2,3])
         it's __repr__ will return "Heap([1,2,3])"
 
         For the Heap, type(self).__name__ will be the string "Heap",
         but for the AVLTree, this expression will be "AVLTree".
-        Using this expression ensures that all subclasses of Heap will have a correct implementation of __repr__,
+        Using this expression ensures that all subclasses of Heap will have a
+        correct implementation of __repr__,
         and that they won't have to reimplement it.
         '''
         return type(self).__name__ + '(' + str(self.to_list('inorder')) + ')'
@@ -46,8 +55,8 @@ class Heap():
         Whenever you implement a data structure,
         the first thing to do is to implement a function that checks whether
         the structure obeys all of its laws.
-        This makes it possible to automatically test whether insert/delete functions
-        are actually working.
+        This makes it possible to automatically test whether insert/delete
+        functions are actually working.
         '''
         if self.root:
             return Heap._is_heap_satisfied(self.root)
@@ -60,6 +69,29 @@ class Heap():
         Implement this method.
         '''
 
+        ret = True
+        #if (must have either 0 or 2 children, except on last row where there
+        #can be at most one node with only a left child but no right child
+        #(filled from left to right))
+        if node.left:
+            if node.value <= node.left:
+                ret &= Heap._is_heap_satisfied(node.left)
+            else:
+                ret = False
+        if node.right:
+            if node.value <= node.right:
+                ret &= Heap._is_heap_satisfied(node.right)
+            else:
+                ret = False
+        return ret
+
+        ret = True
+        height_left = Heap._height(node.left)
+        height_right = Heap._height(node.right)
+                
+        if height_left == height_right:
+            Heap._is_heap_satisfied(node.left)
+
     def insert(self, value):
         '''
         Inserts value into the heap.
@@ -68,17 +100,60 @@ class Heap():
         Implement this function.
 
         HINT:
-        The pseudo code is
-        1. Find the next position in the tree using the binary representation of the total number of nodes
-            1. You will have to explicitly store the size of your heap in a variable (rather than compute it) to maintain the O(log n) runtime
-            1. See https://stackoverflow.com/questions/18241192/implement-heap-using-a-binary-tree for hints
+        The pseudo code is:
+        1. Find the next position in the tree using the binary representation
+        of the total number of nodes
+        1. You will have to explicitly store the size of your heap in a
+        variable (rather than compute it) to maintain the O(log n) runtime
+        1. See
+        https://stackoverflow.com/questions/18241192/implement-heap-using-a
+        -binary-tree for hints
         1. Add `value` into the next position
-        1. Recursively swap value with its parent until the heap property is satisfied
+        1. Recursively swap value with its parent until the heap property is
+        satisfied
 
         HINT:
         Create a @staticmethod helper function,
-        following the same pattern used in the BST and AVLTree insert functions.
+        following the same pattern used in the BST and AVLTree insert
+        functions.
         '''
+
+        size = Heap.__len__() + 1
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            path = Heap._convert_to_binary(size)
+            Heap._insert(value, self.root, path)
+
+    @staticmethod
+    def _insert(value, node, path):
+
+        size = Heap.__len__() + 1
+        path = []
+        while size != 0:
+            bit = num % 2
+            path.insert(0, bit)
+            num = num/2
+            path = path[1:]
+
+        if path[0] == 0:
+            if node.left == None:
+                node.left = Node(value)
+            else:
+                new_node = Heap._insert(value, node.left, path[1:])
+        if path[0] == 1:
+            if node.right == None:
+                 node.right = Node(value)
+            else:
+                new_node = Heap._insert(value, node.right, path[1:])
+        
+        if node.value > new_node.value:
+           temp = node.value
+           node.value = new_node.value
+           new_node.value = temp
+
+        return node.value
+
 
     def insert_list(self, xs):
         '''
@@ -88,6 +163,9 @@ class Heap():
         Implement this function.
         '''
 
+        for x in xs:
+            self.insert(xs)
+
     def find_smallest(self):
         '''
         Returns the smallest value in the tree.
@@ -95,6 +173,19 @@ class Heap():
         FIXME:
         Implement this function.
         '''
+
+        if self.root is None:
+            raise ValueError('Nothing in tree')
+        else:
+            return Heap._find_smallest(node.left)
+
+    @staticmethod
+    def _find_smallest(node):
+        assert node is not None
+        if node.left is None:
+            return node.value
+        else:
+            return Heap._find_smallest(node.left)
 
     def remove_min(self):
         '''
@@ -108,10 +199,59 @@ class Heap():
         The pseudocode is
         1. remove the bottom right node from the tree
         2. replace the root node with what was formerly the bottom right
-        3. "trickle down" the root node: recursively swap it with its largest child until the heap property is satisfied
+        3. "trickle down" the root node: recursively swap it with its largest
+        child until the heap property is satisfied
 
         HINT:
-        I created two @staticmethod helper functions: _remove_bottom_right and _trickle.
+        I created two @staticmethod helper functions: _remove_bottom_right and
+        _trickle.
         It's possible to do it with only a single helper (or no helper at all),
-        but I personally found dividing up the code into two made the most sense.
+        but I personally found dividing up the code into two made the most
+        sense.
         '''
+
+        size = Heap.__len__()
+        Heap._remove_bottom_right(self.root)
+        Heap._trickle(self.root)
+
+    @staticmethod
+    def _remove_bottom_right(node):
+
+        size = Heap.__len__()
+        path = []
+        root = node
+        while size != 0:
+            bit = num % 2
+            path.insert(0, bit)
+            num = num / 2
+        path = path[1:]
+        
+        for i in path[:-1]:
+
+            if i == 0:
+                node = node.left
+            else:
+                node = node.right
+
+        if path[-1] == 0:
+            root.value = node.left.value
+            node.left = None
+        else:
+            root.value = node.right.value
+            node.right = None
+
+    @staticmethod
+    def _trickle(node):
+
+        if node.left and node.right == None:
+            return 
+        elif node.left == None:
+            Heap._trickle(node.right, value)
+        elif node.right == None:
+            Heap._trickle(node.left, value)
+        elif node.left < node.right:
+            Heap._trickle(node.left, value)
+        elif node.right < node.left:
+            Heap._trickle(node.right, value)
+        else:
+            Heap._trickle(node.left, value)
